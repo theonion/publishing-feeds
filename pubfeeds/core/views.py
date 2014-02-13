8f398546d068be729c9d0205740480a5d1fb7cc8
Chris Sinchok <csinchok@theonion.com>
2014-02-13 17:43:53 -0600

    Need to add a feed variable

--- pubfeeds/core/views.py
+++ pubfeeds/core/views.py
@@ -16,7 +16,9 @@ def latest(request, feed_slug):
 
 def edition(request, feed_slug, edition_id):
     edition = get_object_or_404(Edition, id=edition_id)
+    feed = get_object_or_404(Feed, slug=feed_slug)
     context = {
+        "feed": feed,
         "edition": edition,
         "sections": [],
     }
