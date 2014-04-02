31a0f8426dd2462c56cf2870c8c7325948f27b2c
Chris Sinchok <csinchok@theonion.com>
2014-04-02 17:20:57 -0500

    Adding template filter

---
+++ pubfeeds/core/templatetags/core.py
@@ -0,0 +1,16 @@
+from django.utils.encoding import force_text
+from django.utils.safestring import mark_safe
+from django.template.base import Library
+from django.templates.defaultfilters import stringfilter
+
+
+register = Library()
+
+
+@register.filter(is_safe=True)
+@stringfilter
+def escapeamps(value):
+    """
+    Escapes the ampersands in an HTML string.
+    """
+    return mark_safe(force_text(value).replace('&', '&amp;'))
