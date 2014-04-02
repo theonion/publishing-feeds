2e45405282b140b6d202c3b7d2f423b367f78cc3
Chris Sinchok <csinchok@theonion.com>
2014-04-02 11:59:34 -0500

    Switching to a list

--- pubfeeds/core/urls.py
+++ pubfeeds/core/urls.py
@@ -4,6 +4,7 @@ urlpatterns = patterns('pubfeeds.core.views',
     url(r'(?P<feed_slug>[\w\d-]+)/overview$', "feed_overview"),  # noqa
     url(r'(?P<feed_slug>[\w\d-]+)/latest\.xml$', "latest"),
     url(r'(?P<feed_slug>[\w\d-]+)/(?P<edition_id>\d+)/manifest\.xml', "edition"),
+    url(r'(?P<feed_slug>[\w\d-]+)/(?P<edition_id>\d+)/overview', "edition_overview"),
     url(r'(?P<feed_slug>[\w\d-]+)/(?P<edition_id>\d+)/(?P<section_slug>[\w\d-]+)\.xml', "section"),
     url(r'(?P<feed_slug>[\w\d-]+)/(?P<edition_id>\d+)/(?P<section_slug>[\w\d-]+)/(?P<article_id>\d+)\.xml', "article"),
 )
