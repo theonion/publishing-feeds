64b02fdfa9552d2e11dedeba8e775461cc9303a6
Chris Sinchok <csinchok@theonion.com>
2014-04-09 23:43:22 -0500

    Adding redirect URL

--- pubfeeds/urls.py
+++ pubfeeds/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import patterns, include, url
 
+from django.views.generic import RedirectView
+
 from django.contrib import admin
 admin.autodiscover()
 
@@ -9,6 +11,6 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-
+    url(r'^onion/4/manifest\.xml', RedirectView.as_view(url="/onion/5/manifest.xml")),
     url(r'', include('pubfeeds.core.urls'))
 )
