b3262028676db6da396ef17fdec5e194a6b67980
Mike Parent <mparent@umich.edu>
2015-10-16 12:46:48 -0500

    Add production.py to repo

---
+++ pubfeeds/production.py
@@ -0,0 +1,18 @@
+from .settings import *
+
+DEBUG = False
+TEMPLATE_DEBUG = False
+
+ALLOWED_HOSTS = ["pubfeeds.theonion.com"]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'pubfeeds',
+        'USER': 'pubfeeds',
+        'PASSWORD': 'nitffer',
+        "HOST": "192.168.179.247",
+    }
+}
+
+STATIC_URL = 'http://assets.onionstatic.com/pubfeeds/'
